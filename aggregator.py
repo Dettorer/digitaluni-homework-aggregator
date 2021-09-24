8d435ddedc92438a8278e3450f06a4bffa4e7186
Paul Dettorer Hervot <dettorer@dettorer.net>
2021-09-24 23:43:13 +0200

    aggregator: implement a first basic text output

--- aggregator.py
+++ aggregator.py
@@ -1,16 +1,46 @@
+from datetime import datetime
+from html2text import HTML2Text
+from textwrap import shorten
+from typing import Dict, List
 import json
 
 from digitaluni import DigitalUniView
 
 
-if __name__ == "__main__":
-    with DigitalUniView() as view:
-        view.connect("credentials.yml")
-        view.discover_homework()
+def end_datetime(homework: Dict) -> datetime:
+    FMT = "%Y-%m-%d"
+    return datetime.strptime(homework["activite_date_fin"], FMT)
 
-        print("---------")
-        print(view.ue_list)
-        print("---------")
-        print(view.sequences)
-        print("---------")
-        print(json.dumps(view.homework, indent=4))
+
+def text_output(homework_list: List[Dict]) -> None:
+    html_renderer = HTML2Text()
+    homework_list.sort(key=end_datetime)
+
+    for hm in homework_list:
+        name = hm["activite_nom"]
+        end_date = hm["activite_date_fin"]
+        sequence_name = hm["_aggregatorinfo_sequence"]["name"]
+        ue_url = hm["_aggregatorinfo_sequence"]["ue_url"]
+        description = shorten(
+            html_renderer.handle(hm["activite_description"]),
+            width=130,
+        )
+
+        # TODO: color
+        print(f"- avant le {end_date} pour {sequence_name}")
+        print(f"\tLien vers l'{sequence_name[:4]} : {ue_url}")
+        print(f"\t{name}")
+        print(f"\t{description}")
+        print()
+
+
+if __name__ == "__main__":
+    # TODO: don't forget to remove the dump
+    #  with DigitalUniView() as view:
+        #  view.connect("credentials.yml")
+        #  view.discover_homework()
+        #  raw = view.homework
+        #  print(json.dumps(raw, indent=4))
+    with open("dump.json") as f:
+        raw = json.load(f)
+    text_output(raw)
