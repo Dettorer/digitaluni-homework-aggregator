f318fdb4527c2c26d95973c0f57f144738842e40
Paul Dettorer Hervot <dettorer@dettorer.net>
2021-10-19 15:45:11 +0200

    Print dates in a french human readable format
    fixes #5

--- aggregator.py
+++ aggregator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
+from babel.dates import format_date
+from datetime import datetime, date
 from html2text import HTML2Text
 from textwrap import shorten
 from typing import Dict, List
@@ -9,18 +10,19 @@ import json
 from digitaluni import DigitalUniView
 
 
-def end_datetime(homework: Dict) -> datetime:
+def parse_end_date(homework: Dict) -> date:
     FMT = "%Y-%m-%d"
-    return datetime.strptime(homework["activite_date_fin"], FMT)
+    return datetime.strptime(homework["activite_date_fin"], FMT).date()
 
 
 def text_output(homework_list: List[Dict]) -> None:
     html_renderer = HTML2Text()
-    homework_list.sort(key=end_datetime)
+    homework_list.sort(key=parse_end_date)
 
     for hm in homework_list:
         name = hm["activite_nom"]
-        end_date = hm["activite_date_fin"]
+        # human readable date
+        end_date = format_date(parse_end_date(hm), format="full", locale="fr")
         sequence_name = hm["_aggregatorinfo_sequence"]["name"]
         ue_url = hm["_aggregatorinfo_sequence"]["ue_url"]
         description = shorten(
@@ -41,7 +43,7 @@ if __name__ == "__main__":
         view.discover_homework()
         raw = view.homework
         text_output(raw)
-    # TODO: don't forget to remove the dump
     #  with open("dump.json") as f:
         #  raw = json.load(f)
     #  text_output(raw)
+    # TODO: don't forget to remove the dump
